0ec90649ea34ff92befc9f79453cc2ab8e4a0343
Kevin Bren <brenke01@luther.edu>
2014-02-27 23:39:41 -0600

    Fixed prime factors problem

--- primes.js
+++ primes.js
@@ -17,10 +17,46 @@ primeList = function(){
 		 	
 		 }
 		
-		document.write("The primes of " + num + " are: " + lst)
+		return lst
 		 
 
 
 }
 
- 
+isPrime = function(number){
+	for (i=2; i < number; i++){
+		if ((number % i) == 0){
+			return false 
+		}
+	}
+	return true
+}
+
+
+
+var primeFactors = function(){
+	
+	var possiblePrimes = primeList()
+	var primeFactorsList = []
+	var n = 0
+	var primeFound = false 
+	var num = document.getElementById("primeInput").value
+    while (!isPrime(num)){
+
+	   while (n < possiblePrimes.length && !primeFound){
+		  if (num % possiblePrimes[n] == 0){
+			  primeFactorsList.push(possiblePrimes[n])
+		      primeFound = true
+		      num = num / possiblePrimes[n]
+            
+        }
+        n = n + 1
+	}
+	n = 0
+	primeFound = false
+    }
+	primeFactorsList.push(num)
+    document.write("The prime factors are: " + primeFactorsList)
+}
+
+
